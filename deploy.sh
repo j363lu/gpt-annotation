83307161aff77a6fd816187bc8648f081ec32311
j363lu <j363lu@uwaterloo.ca>
2024-09-28 20:13:01 +0800

    update deploy.sh location

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
-scp -r build/* hwadmin@futurescape.uwaterloo.ca:/var/www/html/ai-coder/
-scp .htaccess hwadmin@futurescape.uwaterloo.ca:/var/www/html/ai-coder/
+# scp -r build/* hwadmin@futurescape.uwaterloo.ca:/var/www/html/ai-coder/
+# scp .htaccess hwadmin@futurescape.uwaterloo.ca:/var/www/html/ai-coder/
+
+# new location
+scp -r build/* hwadmin@futurescape.uwaterloo.ca:/var/www/website/ai-coder/
+scp .htaccess hwadmin@futurescape.uwaterloo.ca:/var/www/website/ai-coder/
